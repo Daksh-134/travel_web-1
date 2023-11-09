7c35cdf686f1eed9ad08949c4f3103202a77f3cc
Avaya Aggarwal <119044997+OnePunchMonk@users.noreply.github.com>
2023-11-09 23:29:00 +0530

    process_query

--- process_query.php
+++ process_query.php
@@ -1,39 +1,42 @@
 <?php
+// Enable CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Set content type to JSON
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Return preflight response for OPTIONS requests
+    http_response_code(200);
+    exit();
+}
+
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $mysqli = new mysqli("localhost", "admin", "admin", "travel");
+    $mysqli = new mysqli("localhost", "root", "", "final_db");
 
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
 
     $stmt = $mysqli->prepare("INSERT INTO query (user_name, email, subject, message) VALUES (?, ?, ?, ?)");
-
     $stmt->bind_param("ssss", $name, $email, $subject, $message);
 
     if ($stmt->execute()) {
-
-        // Registration successful
-        echo "Data successfully submitted!";
+        echo json_encode(["message" => "Query sent successfully!"]);
     } else {
-        // Registration failed
-
-        echo "Data successfully submitted!";
-    } else {
-
-        echo "Error: " . $stmt->error;
+        echo json_encode(["message" => "Error: " . $stmt->error]);
     }
 
     $stmt->close();
     $mysqli->close();
 } else {
-    echo "Form not submitted.";
+    echo json_encode(["message" => "Form not submitted."]);
 }
-
 ?>
-
-
